6c5e12312df1426fab642feec9a457525c553c9f
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-08-07 20:45:17 +0530

    added Constructor and more variables

--- src/player.h
+++ src/player.h
@@ -7,26 +7,28 @@
 
 class MusicPlayer{
 public:
+    MusicPlayer();
     void run();
-private:
-    enum Mode { NORMAL, INSERT};
-    Mode currentMode = NORMAL;
-
     void drawBorder();
     void displayInputSection();
     void handleInsertmode();
     void userInput();
     void displayMode();
     void displaySongs();
+    void createSymbol();
+    void updateSymbol();
 
+private:
     int total_height;
     int total_width;
     int input_section_height;
     int display_section_height;
+    std::string currentSymbol;
     std::vector<std::string> song_list;
-
     std::vector<std::string> playlist = {"Song1.mp3", "Song2.mp3"};
     int currentSelection = 0;
+    char symbol = '>';
+    enum Mode { NORMAL, INSERT} currentMode;
 };
 
 #endif
