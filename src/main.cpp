feccfb8bea289c0e32575c0c70d13b81b7505460
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-08-08 15:20:08 +0530

    Added detailed --help section explaining NORMAL and INSERT modes for user interaction.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include "player.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
+void printHelp(){
+    cout << "Usage: groovebox [options]\n"
+         << "\n"
+         << "Modes:\n"
+         << "  [NORMAL] Mode:\n"
+         << "    - Use 'j' and 'k' to scroll through songs in the display section.\n"
+         << "    - Type 'i' to enter INSERT mode.\n"
+         << "\n"
+         << "  [INSERT] Mode:\n"
+         << "    - Type the name of the song you want and hit ENTER to search for that song.\n";
+}
 
-int main(){
+int main(int argc, char* argv[]){
+
+    if(argc > 1){
+        string  arg1 = argv[1];
+        if(arg1 == "--help"){
+            printHelp();
+            return 0;
+        }
+    }
+
     MusicPlayer player;
     player.run();
     return 0;
